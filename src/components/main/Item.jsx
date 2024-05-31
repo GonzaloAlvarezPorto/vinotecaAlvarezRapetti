dbe4e379e0987d1db1ec17df4ee4b8639e423968
Gonzalo Alvarez Porto <gonzaalvarezporto@gmail.com>
2024-05-31 17:43:44 -0300

    Modificaciones para segunda pre entrega

--- src/components/main/Item.jsx
+++ src/components/main/Item.jsx
@@ -7,8 +7,6 @@ export const Item = ({producto}) => {
         (<div className='contenedorProductos__fichaProducto'>
             <h1 className='fichaProducto__titulo'>{producto.nombre}</h1>
             <img src={producto.imagen} alt="" className="fichaProducto__imagen" />
-            <p className="fichaProducto__descripcion">{producto.descripcion}</p>
-            <p className='fichaProducto__precio'>${producto.precio}</p>
             <Link className='fichaProducto__masDetalles' to={`/item/${producto.id}`}>Ver más</Link>
             <Counter/>
         </div>)
--- src/components/main/ItemDetailContainter.jsx
+++ src/components/main/ItemDetailContainter.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import data from "../../data/productos.json"
 
 export const ItemDetailContainter = () => {
@@ -11,6 +11,14 @@ export const ItemDetailContainter = () => {
         setProducto(data.find((prod) => prod.id === parseInt(itemId)));
     }, [itemId])
     return (
-        <div>{producto ? producto.nombre : "Cargando catálogo---"}</div>
+        <div className='cuerpoPrincipal'>
+            <Link to="/" className='cuerpoPrincipal__enlaceRetorno'>Volver a inicio</Link>
+            <div className='cuerpoPrincipal__contenedorProductos'>
+                <div className='contenedorProductos__fichaProducto'>
+                    <h1 className='fichaProducto__titulo'>{producto ? producto.nombre : "Cargando catálogo---"}</h1>
+                    <p className="fichaProducto__descripcion">{producto ? producto.descripcion : "Cargando catálogo---"}</p>
+                </div>
+            </div>
+        </div>
     )
 }
