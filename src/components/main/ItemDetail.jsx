981c597d31e17f33e935884cfaef8b9ecc8101cf
Gonzalo Alvarez Porto <gonzaalvarezporto@gmail.com>
2024-06-18 20:51:51 -0300

    Corrección error 404

--- src/components/main/ItemDetail.jsx
+++ src/components/main/ItemDetail.jsx
@@ -6,7 +6,9 @@ import { doc, getDoc } from 'firebase/firestore';
 
 export const ItemDetail = () => {
 
-    const {agregarAlCarrito} = useContext(CartContext);
+    const { agregarAlCarrito, decrementarCantidad, incrementarCantidad } = useContext(CartContext);
+
+    const [cantidad, setCantidad] = useState(1);
 
     let [producto, setProducto] = useState(undefined);
     let { itemId } = useParams();
@@ -29,7 +31,12 @@ export const ItemDetail = () => {
                     <h1 className='fichaProducto__titulo'>{producto ? producto.nombre : "Cargando catálogo..."}</h1>
                     <img src={producto ? producto.imagen : "Cargando imagen..."} alt="" className="fichaProducto__imagen" />
                     <p className="fichaProducto__descripcion">{producto ? producto.descripcion : "Cargando catálogo..."}</p>
-                    <button onClick={() => agregarAlCarrito(producto)} className='fichaProducto__boton'>Agregar</button>
+                    <div className="fichaProducto__contador">
+                        <button onClick={() => decrementarCantidad(cantidad, setCantidad)} className="contador__botones">➖</button>
+                        <span className="contador__numero">{cantidad}</span>
+                        <button onClick={() => incrementarCantidad(cantidad, setCantidad)} className="contador__botones">➕</button>
+                    </div>
+                    <button onClick={() => agregarAlCarrito(producto, cantidad, setCantidad)} className='fichaProducto__boton'>Agregar</button>
                 </div>
             </div>
         </div>
